2c516c701262ae0a3aaa9740884761c73c210f09
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-11 13:00:55 +0900

    import express-session

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import session from "express-session";
 import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import "./passport";
 const app = express();
 
 // middle ware part.
@@ -20,6 +22,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(localMiddleware); // global routes variables

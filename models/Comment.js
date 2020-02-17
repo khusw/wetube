0e90af6569c1ea27e31e1a1c55132da49795651e
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-17 12:59:50 +0900

    add some column

--- models/Comment.js
+++ models/Comment.js
@@ -4,6 +4,14 @@ const CommentSchema = new mongoose.Schema({
   text: {
     type: String,
     required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }
 });
 

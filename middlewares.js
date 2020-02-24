1c5562cdf2f9efceb67334de32257f1700e36e63
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-24 20:55:58 +0900

    change storage (into aws s3)

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,29 @@
 import multer from "multer";
+import aws from "aws-sdk";
+import multerS3 from "multer-s3";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvatar = multer({ dest: "uploads/avatars/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-2"
+});
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "tube-project/videos"
+  })
+});
+
+const multerAvatar = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "tube-project/avatars"
+  })
+});
 
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";

4341880df3bd477b2d2f7404c24fca0cd2535866
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-25 23:18:52 +0900

    change db directory into cloud

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL_PRODUCTION, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false

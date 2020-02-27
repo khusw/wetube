4b94d50830cca50bf78ebc0e11fe4f4f830dc87e
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-27 20:05:41 +0900

    add avatar defualt

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ import passportLocalMongoose from "passport-local-mongoose";
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  avatarUrl: String,
+  avatarUrl: { type: String, default: process.env.DEFAULT_AVATAR_URL },
   facebookId: Number,
   githubId: Number,
   comments: [

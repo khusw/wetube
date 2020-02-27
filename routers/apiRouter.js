701325d374f864467711be7c81a93dec0bd5afa3
sdy <47293759+sdy199410@users.noreply.github.com>
2020-02-27 20:06:17 +0900

    edit apiRouter

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,7 +3,6 @@ import routes from "../routes";
 import {
   postRegisterView,
   addComment,
-  editComment,
   deleteComment
 } from "../controllers/videoControllers";
 
@@ -11,7 +10,6 @@ const apiRouter = express.Router();
 
 apiRouter.post(routes.registerView, postRegisterView);
 apiRouter.post(routes.addComments, addComment);
-apiRouter.post(routes.editComment, editComment);
-apiRouter.post(routes.deleteComment, deleteComment);
+apiRouter.delete(routes.certainComment, deleteComment);
 
 export default apiRouter;

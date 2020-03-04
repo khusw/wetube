df3776af058ea952b350dfbe10113e8389f09def
sdy <47293759+sdy199410@users.noreply.github.com>
2020-03-04 17:56:51 +0900

    change plugin name

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -44,7 +44,7 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js"
   },
-  plugins: [new ExtractCSS("style.css")],
+  plugins: [new ExtractCSS("styles.css")],
   node: {
     fs: "empty",
     net: "empty"
